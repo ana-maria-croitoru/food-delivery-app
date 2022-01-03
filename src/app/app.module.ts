0ca2f7584a3bfe0c24e1d4ad5375323723535ea6
Ana Croitoru <ana-maria.croitoru@nokia.com>
2022-01-03 22:36:06 +0200

    Create home-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './shared/card/card.component';
 import { RestaurantsComponent } from './features/restaurants/restaurants.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
-  declarations: [AppComponent, CardComponent, RestaurantsComponent],
+  declarations: [AppComponent, CardComponent, RestaurantsComponent, HomePageComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],

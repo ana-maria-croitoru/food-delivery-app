4a039c9c275113d57020a72643586e697b8f97f8
Ana Croitoru <ana-maria.croitoru@nokia.com>
2022-01-04 20:01:16 +0200

    Create meals component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { CardComponent } from './shared/card/card.component';
 import { RestaurantsComponent } from './features/restaurants/restaurants.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MealsComponent } from './features/meals/meals.component';
 
 @NgModule({
-  declarations: [AppComponent, CardComponent, RestaurantsComponent, HomePageComponent],
+  declarations: [AppComponent, CardComponent, RestaurantsComponent, HomePageComponent, MealsComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
